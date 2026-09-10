4c8623175a119d2f58fbb64c321642437e7cf0a6
Henjy007 <henjy007@gmail.com>
2026-09-10 16:20:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let historyIndex = -1;
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-JavaScript
 // Load your uploaded audio file
 const keyAudio = new Audio('366f2799-7060-488e-9d1b-f8852a946afd-render.mp3'); 
 
