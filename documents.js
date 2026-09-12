ae704e6ae6014ec8e26048dd0ed17b1bef3b1c8d
Henjy007 <henjy007@gmail.com>
2026-09-12 14:27:12 +0300

    Test

--- documents.js
+++ documents.js
@@ -5,10 +5,10 @@ const DOCUMENTS = {
   "test_001": {
     archivedOn: "09/09/2026",
     content: [
-      { type: "text", value: "PARAGON RESEARCH INSTITUTE - CLASSIFIED RECORD" },
+      { type: "text", value: "PARAGON RESEARCH INSTITUTE - UNCLASSIFIED RECORD" },
       { type: "text", value: "TITLE - BASIC TEST 001" },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "Images/Test_001.png", alt: "Classified Image" },
+      { type: "image", url: "https://imgur.com/a/ES9bt5N", alt: "test_001" }
       { type: "pause", duration: 1000 },
       { type: "video", url: "https://medal.tv/clip/nvnogn9UZwgK_ya89/embed" },
       { type: "pause", duration: 1000 },
