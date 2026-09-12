eadf06da728f4bf37e4c149b034b4bfe3effa1aa
Henjy007 <henjy007@gmail.com>
2026-09-12 20:25:01 +0300

    Update documents.js

--- documents.js
+++ documents.js
@@ -23,8 +23,7 @@ const DOCUMENTS = {
       { type: "text", value: "PARAGON RESEARCH INSTITUTE - UNCLASSIFIED RECORD" },
       { type: "text", value: "TITLE - PLACEHOLDER" },
       { type: "pause", duration: 1000 },
-      { type: "video", // Uses iframe under the hood in your script
-        url: "https://docs.google.com/document/d/14fDPWCguIFOlaAD6rkCCtMgKZzVlTFoikuNM9G1EcsA/preview" },
+      { type: "doc", url: "https://docs.google.com/document/d/YOUR_DOC_ID/preview" },
       { type: "pause", duration: 1000 },
       { type: "text", value: "End of document." },
       { type: "text", value: " " }
