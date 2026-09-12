ace844efdd62404e20bbaab2114316d06c86b13d
Henjy007 <henjy007@gmail.com>
2026-09-12 20:07:27 +0300

    Added document 'placeholder'

--- documents.js
+++ documents.js
@@ -3,7 +3,7 @@
 // ==========================================
 const DOCUMENTS = {
   "test_001": {
-    archivedOn: "09/09/2026",
+    archivedOn: "09/09/19XX",
     content: [
       { type: "text", value: "PARAGON RESEARCH INSTITUTE - UNCLASSIFIED RECORD" },
       { type: "text", value: "TITLE - BASIC TEST 001" },
@@ -16,4 +16,20 @@ const DOCUMENTS = {
       { type: "text", value: " " }
     ]
   },
+};
+
+const DOCUMENTS = {
+  "placeholder": {
+    archivedOn: "12/09/19XX",
+    content: [
+      { type: "text", value: "PARAGON RESEARCH INSTITUTE - UNCLASSIFIED RECORD" },
+      { type: "text", value: "TITLE - PLACEHOLDER" },
+      { type: "pause", duration: 1000 },
+      { type: "video", // Uses iframe under the hood in your script
+        url: "https://docs.google.com/document/d/14fDPWCguIFOlaAD6rkCCtMgKZzVlTFoikuNM9G1EcsA/edit?tab=t.0" }
+      { type: "pause", duration: 1000 },
+      { type: "text", value: "End of document." },
+      { type: "text", value: " " }
+    ]
+  }
 };
