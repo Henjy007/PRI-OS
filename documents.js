3b9ad53b5cd91a2c23b6dee9e28177e4b86d4e6a
Henjy007 <henjy007@gmail.com>
2026-09-15 16:07:04 +0300

    test_002

--- documents.js
+++ documents.js
@@ -23,32 +23,24 @@ const DOCUMENTS = {
       { type: "text", value: "PARAGON RESEARCH INSTITUTE - UNCLASSIFIED RECORD" },
       { type: "text", value: "TITLE - ADVANCED TEST 002" },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "https://i.imgur.com/2xGgnWI.jpeg", alt: "test_002-photo1" },
+      { type: "image", url: "https://i.postimg.cc/4nFdp6cM/PRI-Advanced-test-002-images-0.jpg", alt: "test_002-photo1" },
       { type: "text", value: " " },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "https://i.imgur.com/pnvghbZ.jpeg", alt: "test_002-photo2" },
+      { type: "image", url: "https://i.postimg.cc/Z0qn979m/PRI-Advanced-test-002-images-1.jpg", alt: "test_002-photo2" },
       { type: "text", value: " " },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "https://i.imgur.com/9IkWSIW.jpeg", alt: "test_002-photo3" },
+      { type: "image", url: "https://i.postimg.cc/yWNxJQJH/PRI-Advanced-test-002-images-2.jpg", alt: "test_002-photo3" },
       { type: "text", value: " " },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "https://i.imgur.com/6cGxL4E.jpeg", alt: "test_002-photo4" },
+      { type: "image", url: "https://i.postimg.cc/Cd61bG8Q/PRI-Advanced-test-002-images-3.jpg", alt: "test_002-photo4" },
       { type: "text", value: " " },
       { type: "pause", duration: 1000 },
       { type: "video", url: "https://medal.tv/clip/nwaU4cvIRV-0pP82T/embed" },
       { type: "text", value: " " },
       { type: "pause", duration: 1000 },
-      { type: "image", url: "https://i.imgur.com/lejdrPk.jpeg", alt: "test_002-photo5" },
+      { type: "image", url: "https://i.postimg.cc/vDmT1q1d/PRI-Advanced-test-002-images-4.jpg", alt: "test_002-photo5" },
       { type: "text", value: " " }
     ]
   },
 
-  "temp": {
-    archivedOn: "13/09/19XX",
-    content: [
-      { type: "text", value: "this is for you samg10018" },
-      { type: "image", url: "https://i.postimg.cc/mDQWwLQJ/Screenshot-2026-09-14-200655.png", alt: "N/A" }
-      ]
-    }
-      
 }
